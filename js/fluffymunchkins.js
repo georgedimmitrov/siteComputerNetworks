8d62a328e8143d1825ccfe299d2748459015c67b
georgedimmitrov <georgedimmitrov@gmail.com>
2017-01-09 14:39:41 +0200

    first commit

---
+++ js/fluffymunchkins.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  $('[data-toggle="popover"]').popover(); 
+});
+
+// Only enable if the document has a long scroll bar
+// Note the window height + offset
+if ( ($(window).height() + 100) < $(document).height() ) {
+    $('#top-link-block').removeClass('hidden').affix({
+        // how far to scroll down before link "slides" into view
+        offset: {top:100}
+    });
+}
